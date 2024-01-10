4b92742e77092a0a12ea68b2922a9bf3a2a9d498
Sanjay Kumar <72185623+sanjayhjp121@users.noreply.github.com>
2024-01-10 17:24:00 +0530

    Add files via upload

---
+++ test-fxn.js
@@ -0,0 +1,10 @@
+const AWS = require('aws-sdk');
+const ec2 = new AWS.EC2();
+
+exports.handler = async (event, context) => {
+    const params = {
+        InstanceIds: ['i-0a2d53a92dbdbca00']
+    };
+
+    await ec2.startInstances(params).promise();
+};
